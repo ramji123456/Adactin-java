f3bb57bcdff19f323cbec81e1b13ab979c9c2480
ramji123456 <t.ramji1994@gmail.com>
2022-06-11 14:41:54 +0530

    javafile ready

---
+++ Class1.java
@@ -0,0 +1,19 @@
+package com.project1;
+
+public class Class1 {
+	public void name () {
+		System.out.println("ramji");
+	}
+	public void age() {
+		System.out.println(23);
+	}
+	
+
+	public static void main(String[] args) {
+	Class1 rn = new Class1();
+	rn.name();
+	rn.age();
+
+	}
+
+}
